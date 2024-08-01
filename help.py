c89a33d600e765a6f74c0a80ea2ebb60d153282b
Levi-cell <levigov21@gmail.com>
2024-08-01 17:39:21 -0300

    Subindo arquivos

---
+++ help.py
@@ -0,0 +1,21 @@
+from manipulandoBanco import adicionaFaturamento as af, adicionaCusto as ac
+from tratandoErros import *
+
+# Execute esse arquivo de forma separada caso precise de ajuda
+
+help(adiciona_faturamento)
+help(trata_entrada_de_opcao)
+help(trata_entrada_de_valor)
+help(calcula_ultimo_periodo_mensal)
+help(calcula_ultimo_periodo_semanal)
+help(calcula_proximo_periodo_semanal)
+help(primeiro_dia_ano_passado)
+help(ultimo_periodo_anual)
+help(ultimo_dia_ano_passado)
+help(verifica_proxima_ter)
+help(verificar_ter_passada)
+help(obter_primeiro_dia_mes_passado)
+help(obter_ultimo_dia_mes_passado)
+help(tratamento_de_retorno)
+
+# help(trata_entrada_de_letra)
